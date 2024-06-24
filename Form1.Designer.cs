93af24fb9e491f8db1fa578ff32573be5cb3d949
Administrator <Administrator@LAB1-MAY15>
2024-06-24 16:29:36 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace CS_QuanLiThongTin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.maTen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.hoTen = new System.Windows.Forms.TextBox();
+            this.namSinh = new System.Windows.Forms.TextBox();
+            this.queQuan = new System.Windows.Forms.TextBox();
+            this.tentimkiem = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnFix = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 180);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(939, 240);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // maTen
+            // 
+            this.maTen.Location = new System.Drawing.Point(158, 25);
+            this.maTen.Name = "maTen";
+            this.maTen.Size = new System.Drawing.Size(347, 20);
+            this.maTen.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mã Tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Năm Sinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(45, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 14);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Quê Quán";
+            // 
+            // hoTen
+            // 
+            this.hoTen.Location = new System.Drawing.Point(158, 54);
+            this.hoTen.Name = "hoTen";
+            this.hoTen.Size = new System.Drawing.Size(347, 20);
+            this.hoTen.TabIndex = 1;
+            // 
+            // namSinh
+            // 
+            this.namSinh.Location = new System.Drawing.Point(158, 84);
+            this.namSinh.Name = "namSinh";
+            this.namSinh.Size = new System.Drawing.Size(347, 20);
+            this.namSinh.TabIndex = 1;
+            // 
+            // queQuan
+            // 
+            this.queQuan.Location = new System.Drawing.Point(158, 115);
+            this.queQuan.Name = "queQuan";
+            this.queQuan.Size = new System.Drawing.Size(347, 20);
+            this.queQuan.TabIndex = 1;
+            // 
+            // tentimkiem
+            // 
+            this.tentimkiem.Location = new System.Drawing.Point(665, 137);
+            this.tentimkiem.Name = "tentimkiem";
+            this.tentimkiem.Size = new System.Drawing.Size(341, 20);
+            this.tentimkiem.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(1050, 23);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 51);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(1050, 80);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(97, 51);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(1050, 137);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(97, 51);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnFix
+            // 
+            this.btnFix.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFix.Location = new System.Drawing.Point(1050, 194);
+            this.btnFix.Name = "btnFix";
+            this.btnFix.Size = new System.Drawing.Size(97, 51);
+            this.btnFix.TabIndex = 3;
+            this.btnFix.Text = "Fix";
+            this.btnFix.UseVisualStyleBackColor = true;
+            this.btnFix.Click += new System.EventHandler(this.btnFix_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(1050, 251);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 51);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(1050, 308);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(97, 51);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturn.Location = new System.Drawing.Point(1050, 365);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(97, 51);
+            this.btnReturn.TabIndex = 3;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 450);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnFix);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.queQuan);
+            this.Controls.Add(this.namSinh);
+            this.Controls.Add(this.hoTen);
+            this.Controls.Add(this.tentimkiem);
+            this.Controls.Add(this.maTen);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox maTen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox hoTen;
+        private System.Windows.Forms.TextBox namSinh;
+        private System.Windows.Forms.TextBox queQuan;
+        private System.Windows.Forms.TextBox tentimkiem;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnFix;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnReturn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CS_QuanLiThongTin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=LAB1-MAY15\MISASME2022;Initial Catalog=QuanLiThongTin;Integrated Security=True;Encrypt=False");
+        private void openCon()
+        {
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+            }
+        }
+
+        private void closeCon()
+        {
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        private Boolean Exe(string cmd)
+        {
+            openCon();
+            Boolean check;
+            try
+            {
+                SqlCommand sc = new SqlCommand(cmd, con);
+                sc.ExecuteNonQuery();
+                check = true;
+            }
+            catch (Exception)
+            {
+                check = false;
+                throw;
+            }
+            closeCon();
+            return check;
+        }
+
+        private DataTable Red(string cmd)
+        {
+            openCon();
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand sc = new SqlCommand(cmd, con);
+                SqlDataAdapter sda = new SqlDataAdapter(sc);
+                sda.Fill(dt);
+            }
+            catch (Exception)
+            {
+                dt = null;
+                throw;
+            }
+            closeCon();
+            return dt;
+        }
+
+        private void load()
+        {
+            DataTable dt = Red("SELECT * FROM QUANLITHONGTIN");
+            if (dt != null)
+            {
+                dataGridView1.DataSource = dt;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            maTen.ResetText();
+            hoTen.ResetText();
+            namSinh.ResetText();
+            queQuan.ResetText();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Exe("INSERT INTO QUANLITHONGTIN(MATT, HOTEN, NAMSINH, QUEQUAN) VALUES (N'" + maTen.Text + "', N'" + hoTen.Text + "', N'" + namSinh.Text + "', N'" + queQuan.Text + "') ");
+            //Exe("INSERT INTO QUANLITHONGTIN(MATT, HOTEN, NAMSINH, QUEQUAN) VALUES (N'ma',N'ten',N'nam',N'que') ");
+            load();
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void btnFix_Click(object sender, EventArgs e)
+        {
+            Exe("UPDATE QUANLITHONGTIN SET HOTEN = N'" + hoTen.Text + "', NAMSINH =  N'" + namSinh.Text + "', QUEQUAN = N'" + queQuan.Text + "' WHERE MATT = '" + maTen.Text + "' ");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            maTen.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            hoTen.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            namSinh.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            queQuan.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Exe("DELETE FROM QUANLITHONGTIN WHERE MATT = '" + maTen.Text + "'");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            DataTable dt = Red("SELECT * FROM QUANLITHONGTIN WHERE MATT = '" + tentimkiem.Text + "' ");
+            if (dt != null)
+            {
+                dataGridView1.DataSource = dt;
+            }
+        }
+    }
+}
